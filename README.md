03b576b31d1b3bac9151546d90ab38f2894e3f60
rachelchss <114247822+rachelchss@users.noreply.github.com>
2022-09-25 21:40:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-vhcq9c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-vhcq9c)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+npm install -g @angular/cli
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
